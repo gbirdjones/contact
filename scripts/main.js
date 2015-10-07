46dc3378b9d07d7e28a18b7b92a97d8250b72a06
Gabriel Bird <drmonumbo@gmail.com>
2015-10-07 12:35:13 -0500

    complete

--- scripts/main.js
+++ scripts/main.js
@@ -17,7 +17,7 @@ error4.hide();
 
 var butt = $('#butt');
 
-var store = [];
+var store = {};
 
 butt.on('click', storeInfo);
 
@@ -29,33 +29,46 @@ function storeInfo(e) {
 	var box4 = $('#box4');
 
 	
-	store.push(box1.val());
+	store.name = box1.val();
+	console.log(store.name)
 	
-	store.push(box2.val());
+	store.email = box2.val();
+	console.log(store.email)
 
-	store.push(box3.val());
+	store.website = box3.val();
+	console.log(store.website)
 	
-	store.push(box4.val());
+	store.message = box4.val();
+	console.log(store.message)
 
-	if (!store[0]) {
-		error1.show();
-		store = [];
-	}
-	if (store[1].indexOf('@') == -1) {
+	
+	if (store.email.indexOf('@') == -1) {
 		error2.show();
-		store = [];
 	}
-	if (store[2].indexOf('http://') == -1) {
+	else if (store.website.indexOf('http://') == -1) {
 		error3.show();
-		store = [];
 	}
-	if (!store[3]) {
-		error4.show();
-		store = [];
+	else if (store.name === '') {
+		console.log('name undefined');
+		error1.show();
 	}
-	sucInner.html('thanks ' + store[0] + ' we have received your info');
+	else if (store.email === '') {
+		console.log('email undefined');
+		error1.show();
+	}
+	else if (store.website === '') {
+		console.log('website undefined');
+		error1.show();
+	}
+	else if (store.message === '') {
+		console.log('message undefined');
+		error1.show();
+	} else {
+	console.log(store.name);
+	sucInner.html('thanks ' + store.name + ' we have received your info');
 	success.show();
 	sucInner.show();
+}
 
 
 
